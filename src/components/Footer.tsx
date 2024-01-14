8e449d65e46c96c641038ff2376fc43bba6aff8b
Víctor Olvera <vhocar@gmail.com>
2024-01-14 14:03:23 -0700

    Fixed footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,9 +5,9 @@ const Footer = () => {
     return (
         <div className={'flex flex-col bg-white justify-center fixed bottom-0 h-24 w-full text-slate-800 items-center'}>
             
-            <div className="flex flex-wrap justify-center gap-2 p-2">
+            <div className="flex flex-wrap items-center justify-center gap-x-2 p-0 sm:p-2">
 
-                <h3 className="text-center">¿Quieres un catálogo como éste?</h3>
+                <h3 className="text-center xs:text-xs sm:text-base">¿Quieres un catálogo como éste?</h3>
                 <div className={'text-sky-600 font-bold text-center'}>
                     <a href="https://milist.app" target="_blank">
                         ¡Visítanos y descarga la app!
